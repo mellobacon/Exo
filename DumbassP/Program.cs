6b7cc38ad4e096f000827bf4589878b9618a18dc
mellobacon <liz6757@outlook.com>
2021-11-14 20:39:38 -0600

    inital commit

---
+++ DumbassP/Program.cs
@@ -0,0 +1,16 @@
+﻿using DumbassP.Compiler;
+
+namespace DumbassP
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var repl = new Repl()
+            {
+                Prompt = "» "
+            };
+            repl.Run();
+        }
+    }
+}
