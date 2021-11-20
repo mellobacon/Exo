f72d34248f2b2d0cef0be991dc3f1221436f61bc
mellobacon <42365887+mellobacon@users.noreply.github.com>
2021-11-19 21:07:37 -0600

    e

--- DumbassP.Compiler/CodeAnalysis/Lexer/SyntaxToken.cs
+++ DumbassP.Compiler/CodeAnalysis/Lexer/SyntaxToken.cs
@@ -1,10 +1,18 @@
-﻿namespace DumbassP.Compiler.CodeAnalysis.Lexer
+﻿using System.Collections.Generic;
+using System.Linq;
+using DumbassP.Compiler.CodeAnalysis.Parser;
+
+namespace DumbassP.Compiler.CodeAnalysis.Lexer
 {
-    public class SyntaxToken
+    public class SyntaxToken : SyntaxNode
     {
         public string Text;
         public object Value;
-        public SyntaxTokenType Type;
+        public override SyntaxTokenType Type { get; }
+        public override IEnumerable<SyntaxNode> GetChildren()
+        {
+            return Enumerable.Empty<SyntaxNode>(); // Theres no node to return so fuck off
+        }
 
         public SyntaxToken(string text, object value, SyntaxTokenType type)
         {
