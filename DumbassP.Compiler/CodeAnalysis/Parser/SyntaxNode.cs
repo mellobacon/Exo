e94faea72a6198440f68eab5a5454c01ebc84ea0
mellobacon <42365887+mellobacon@users.noreply.github.com>
2021-11-19 21:04:36 -0600

    Added stuff for the syntax tree

---
+++ DumbassP.Compiler/CodeAnalysis/Parser/SyntaxNode.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using DumbassP.Compiler.CodeAnalysis.Lexer;
+
+namespace DumbassP.Compiler.CodeAnalysis.Parser
+{
+    // This is here to group everything i need to print the syntax tree. The type is there because i need to access it
+    // for the tree too. The GetChildren method is there because i need a way to collect all the nodes for the tree
+    public abstract class SyntaxNode
+    {
+        public abstract SyntaxTokenType Type { get; }
+
+        public abstract IEnumerable<SyntaxNode> GetChildren();
+    }
+}
---
+++ DumbassP.Compiler/CodeAnalysis/Parser/SyntaxTree.cs
@@ -0,0 +1,23 @@
+﻿using DumbassP.Compiler.CodeAnalysis.Lexer;
+using DumbassP.Compiler.CodeAnalysis.Parser.Expressions;
+
+namespace DumbassP.Compiler.CodeAnalysis.Parser
+{
+    public class SyntaxTree
+    {
+        public ExpressionSyntax Root;
+        private SyntaxToken _eofToken;
+
+        public SyntaxTree(ExpressionSyntax expression, SyntaxToken eofToken)
+        {
+            Root = expression;
+            _eofToken = eofToken;
+        }
+
+        public static SyntaxTree Parse(string text)
+        {
+            Parser parser = new Parser(text);
+            return parser.Parse();
+        }
+    }
+}
