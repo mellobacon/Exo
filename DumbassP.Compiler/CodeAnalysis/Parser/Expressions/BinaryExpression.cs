eeadaf4a0113a964673a60dc90e9ec6d4d58fcf9
mellobacon <42365887+mellobacon@users.noreply.github.com>
2021-11-19 21:04:09 -0600

    Added expressions

---
+++ DumbassP.Compiler/CodeAnalysis/Parser/Expressions/BinaryExpression.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using DumbassP.Compiler.CodeAnalysis.Lexer;
+
+namespace DumbassP.Compiler.CodeAnalysis.Parser.Expressions
+{
+    public class BinaryExpression : ExpressionSyntax
+    {
+        private ExpressionSyntax left;
+        private SyntaxToken op;
+        private ExpressionSyntax right;
+
+        public BinaryExpression(ExpressionSyntax left, SyntaxToken op, ExpressionSyntax right)
+        {
+            this.left = left;
+            this.op = op;
+            this.right = right;
+        }
+
+        public override SyntaxTokenType Type => SyntaxTokenType.BinaryExpression;
+        public override IEnumerable<SyntaxNode> GetChildren()
+        {
+            yield return left;
+            yield return op;
+            yield return right;
+        }
+    }
+}
---
+++ DumbassP.Compiler/CodeAnalysis/Parser/Expressions/LiteralExpression.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using DumbassP.Compiler.CodeAnalysis.Lexer;
+
+namespace DumbassP.Compiler.CodeAnalysis.Parser.Expressions
+{
+    public class LiteralExpression : ExpressionSyntax
+    {
+        private SyntaxToken token;
+
+        public LiteralExpression(SyntaxToken token)
+        {
+            this.token = token;
+        }
+
+        public override SyntaxTokenType Type => SyntaxTokenType.LiteralExpression;
+        public override IEnumerable<SyntaxNode> GetChildren()
+        {
+            yield return token;
+        }
+    }
+}
