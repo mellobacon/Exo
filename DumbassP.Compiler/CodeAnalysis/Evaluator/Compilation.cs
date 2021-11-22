9f75603f1d7001965662833d4f539c008d239dba
mellobacon <42365887+mellobacon@users.noreply.github.com>
2021-11-21 19:52:32 -0600

    Added better way to handle the result

---
+++ DumbassP.Compiler/CodeAnalysis/Evaluator/Compilation.cs
@@ -0,0 +1,28 @@
+﻿using DumbassP.Compiler.CodeAnalysis.Errors;
+using DumbassP.Compiler.CodeAnalysis.Parser;
+using DumbassP.Compiler.CodeAnalysis.Parser.Expressions;
+
+namespace DumbassP.Compiler.CodeAnalysis.Evaluator
+{
+    public class Compilation
+    {
+        private object Value;
+        private SyntaxTree _tree;
+        public Compilation(SyntaxTree tree)
+        {
+            _tree = tree;
+        }
+        
+        public Result Evaluate()
+        {
+            Evaluator evaluator = new Evaluator(_tree.Root);
+            Value = evaluator.Evaluate();
+
+            if (_tree.Errors.Any())
+            {
+                return new Result(_tree.Errors, null);
+            }
+            return new Result(_tree.Errors, Value);
+        }
+    }
+}
---
+++ DumbassP.Compiler/CodeAnalysis/Evaluator/Result.cs
@@ -0,0 +1,16 @@
+﻿using DumbassP.Compiler.CodeAnalysis.Errors;
+using DumbassP.Compiler.CodeAnalysis.Parser.Expressions;
+
+namespace DumbassP.Compiler.CodeAnalysis.Evaluator
+{
+    public class Result
+    {
+        public ErrorList Errors;
+        public object Value;
+        public Result(ErrorList errors, object value)
+        {
+            Errors = errors;
+            Value = value;
+        }
+    }
+}
