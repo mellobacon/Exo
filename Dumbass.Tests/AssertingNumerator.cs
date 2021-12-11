29f3529f4e6dc5cd7e43c9b974ead1c04e2523b3
mellobacon <liz6757@outlook.com>
2021-12-11 04:38:03 -0600

    Added testing for the parser

---
+++ Dumbass.Tests/AssertingNumerator.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DumbassP.Compiler.CodeAnalysis.Lexer;
+using DumbassP.Compiler.CodeAnalysis.Parser;
+using Xunit;
+
+namespace Dumbass.Tests
+{
+    public class AssertingNumerator : IDisposable
+    {
+        readonly IEnumerator<SyntaxNode> _Enumerator;
+        private bool HasErrors;
+
+        public AssertingNumerator(SyntaxNode node)
+        {
+            // Get the list of nodes
+            _Enumerator = Flatten(node).GetEnumerator();
+        }
+
+        private bool MarkFailed()
+        {
+            HasErrors = true;
+            return false;
+        }
+
+        // This gets all the nodes from the tree
+        private static IEnumerable<SyntaxNode> Flatten(SyntaxNode node)
+        {
+            var stack = new Stack<SyntaxNode>();
+            stack.Push(node); // push the root to the stack
+
+            while (stack.Count > 0)
+            {
+                var n = stack.Pop(); // the root or subroot node
+                yield return n;
+                // get each child of each root. reversed because yes. what am i smart to you?
+                foreach (var child in n.GetChildren().Reverse())
+                {
+                    stack.Push(child);   
+                }
+            }
+        }
+
+        public void AssertNode(SyntaxTokenType type)
+        {
+            try
+            {
+                Assert.True(_Enumerator.MoveNext());
+                if (_Enumerator.Current != null)
+                {
+                    Assert.Equal(type, _Enumerator.Current.Type);
+                    Assert.IsNotType<SyntaxToken>(_Enumerator.Current);
+                }
+            }
+            catch when (MarkFailed())
+            {
+                throw;
+            }
+        }
+        
+        public void AssertToken(SyntaxTokenType type, string text)
+        {
+            try
+            {
+                Assert.True(_Enumerator.MoveNext());
+                if (_Enumerator.Current != null)
+                {
+                    Assert.Equal(type, _Enumerator.Current.Type);
+                    var token = Assert.IsType<SyntaxToken>(_Enumerator.Current);
+                    Assert.Equal(text, token.Text);
+                }
+            }
+            catch when (MarkFailed())
+            {
+                throw;
+            }
+        }
+
+        public void Dispose()
+        {
+            if (!HasErrors)
+            {
+                Assert.False(_Enumerator.MoveNext());   
+            }
+            _Enumerator.Dispose();
+        }
+    }
+}
--- Dumbass.Tests/ParserTest.cs
+++ Dumbass.Tests/ParserTest.cs
@@ -33,11 +33,53 @@ namespace Dumbass.Tests
             }
         }
 
+        private static ExpressionSyntax ParseExpression(string text)
+        {
+            var tree = SyntaxTree.Parse(text);
+            var root = tree.Root;
+            return root;
+        }
+
         [Theory]
         [MemberData(nameof(BinaryOpData))]
-        public static void Parser_Test()
+        public static void Parser_Honors_Precedence(SyntaxTokenType type1, SyntaxTokenType type2)
         {
+            var typeprecedence1 = SyntaxPrecedence.GetBinaryPrecedence(type1);
+            var typeprecedence2 = SyntaxPrecedence.GetBinaryPrecedence(type2);
+            var typetext1 = SyntaxPrecedence.GetText(type1);
+            var typetext2 = SyntaxPrecedence.GetText(type2);
+
+            var text = $"1 {typetext1} 2 {typetext2} 3";
+            var expression = ParseExpression(text);
             
+            // What the tree should look like depending on precedence
+            using var e = new AssertingNumerator(expression);
+            if (typeprecedence2 > typeprecedence1)
+            {
+                e.AssertNode(SyntaxTokenType.BinaryExpression);
+                    e.AssertNode(SyntaxTokenType.LiteralExpression);
+                        e.AssertToken(SyntaxTokenType.NumberToken, "1");
+                e.AssertToken(type1, typetext1);
+                e.AssertNode(SyntaxTokenType.BinaryExpression);
+                    e.AssertNode(SyntaxTokenType.LiteralExpression);
+                        e.AssertToken(SyntaxTokenType.NumberToken, "2");
+                    e.AssertToken(type2, typetext2);
+                    e.AssertNode(SyntaxTokenType.LiteralExpression);
+                        e.AssertToken(SyntaxTokenType.NumberToken, "3");
+            }
+            else
+            {
+                e.AssertNode(SyntaxTokenType.BinaryExpression);
+                    e.AssertNode(SyntaxTokenType.BinaryExpression);
+                        e.AssertNode(SyntaxTokenType.LiteralExpression);
+                            e.AssertToken(SyntaxTokenType.NumberToken, "1");
+                        e.AssertToken(type1, typetext1);
+                        e.AssertNode(SyntaxTokenType.LiteralExpression);
+                            e.AssertToken(SyntaxTokenType.NumberToken, "2");
+                        e.AssertToken(type2, typetext2);
+                        e.AssertNode(SyntaxTokenType.LiteralExpression);
+                            e.AssertToken(SyntaxTokenType.NumberToken, "3");   
+            }
         }
     }
 }
