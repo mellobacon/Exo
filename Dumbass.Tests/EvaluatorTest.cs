dfed3e615d8e7cd50fddb2698c1a7ba9e3f18fdc
mellobacon <42365887+mellobacon@users.noreply.github.com>
2021-12-12 00:56:45 -0600

    Added tests for the evaluator

---
+++ Dumbass.Tests/EvaluatorTest.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using DumbassP.Compiler.CodeAnalysis.Evaluator;
+using DumbassP.Compiler.CodeAnalysis.Parser;
+using Xunit;
+
+namespace Dumbass.Tests
+{
+    public static class EvaluatorTest
+    {
+        private static IEnumerable<object[]> GetEvaluations()
+        {
+            var evals = new (string text, object value)[]
+            {
+                ("1 + 2 + 3", 6),
+                ("1 + 2 * 3", 7),
+                ("(1 + 2) * 3", 9),
+                ("1 / 2", 0.5f),
+                ("1 % 2", 1),
+                ("False", false),
+                ("True", true),
+                ("False || True", true),
+                ("True && True", true),
+                ("True && False", false)
+            };
+            foreach (var (text, value) in evals)
+            {
+                yield return new[] { text, value };
+            }
+        }
+        [Theory]
+        [MemberData(nameof(GetEvaluations))]
+        public static void Evaluator_Outputs_Correct_Value(string text, object value)
+        {
+            SyntaxTree tree = SyntaxTree.Parse(text);
+            Compilation compilation = new Compilation(tree);
+            Result result = compilation.Evaluate();
+            Assert.Equal(value, result.Value);
+        }
+    }
+}
