09747c35d91bfd1b0598df857e510053d844b061
mellobacon <liz6757@outlook.com>
2022-01-04 08:20:24 -0600

    Added position and textspan calculation

--- DumbassP.Compiler/CodeAnalysis/Lexer/SyntaxToken.cs
+++ DumbassP.Compiler/CodeAnalysis/Lexer/SyntaxToken.cs
@@ -7,14 +7,17 @@ namespace DumbassP.Compiler.CodeAnalysis.Lexer
     public class SyntaxToken : SyntaxNode
     {
         public string Text;
-        public object Value;
+        public readonly object Value;
+        public int Position;
+        public TextSpan TextSpan => new(Position, Text.Length);
         public override SyntaxTokenType Type { get; }
 
-        public SyntaxToken(string text, object value, SyntaxTokenType type)
+        public SyntaxToken(string text, object value, SyntaxTokenType type, int position)
         {
             Text = text;
             Value = value;
             Type = type;
+            Position = position;
         }
         
         public override IEnumerable<SyntaxNode> GetChildren()
