1fcb87bf18328ac19ea60ccfb41851165a8d692d
mellobacon <liz6757@outlook.com>
2022-01-04 08:19:16 -0600

    Added textspan to track the position of errors

---
+++ DumbassP.Compiler/CodeAnalysis/TextSpan.cs
@@ -0,0 +1,14 @@
+﻿namespace DumbassP.Compiler.CodeAnalysis
+{
+    public struct TextSpan
+    {
+        public int Start;
+        public int Length;
+        public int End => Start + Length;
+        public TextSpan(int start, int length)
+        {
+            Start = start;
+            Length = length;
+        }
+    }
+}
